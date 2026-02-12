d536b8245b88c65a872166b38370e46c747c2a81
yashapex <104975493+yashapex@users.noreply.github.com>
2026-02-12 07:09:28 +0530

    Add import for Value annotation in WebSecurityConfig

--- src/main/java/hiring_project/shadow_assistant/security/WebSecurityConfig.java
+++ src/main/java/hiring_project/shadow_assistant/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package hiring_project.shadow_assistant.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
